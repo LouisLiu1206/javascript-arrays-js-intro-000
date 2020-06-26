2198a3c02acfd7b4bd106e4574759c62f7413240
Qize <louisliu1206@yahoo.com>
2020-06-26 22:49:59 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,10 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray(array, element) {
-  return array.unshift("element");
+  return [element, ...array];
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift("element")
+  return array;
 }
